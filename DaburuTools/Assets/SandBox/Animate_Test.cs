fee56151208a8468b5e54f026de6319aa24f320b
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-02-16 10:55:57 +0800

    .gitignore is now working for real this time.

---
+++ DaburuTools/Assets/SandBox/Animate_Test.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using DaburuTools.Animate;
+
+public class Animate_Test : MonoBehaviour {
+
+	Animate mAnimate;
+
+	void Awake()
+	{
+		mAnimate = new Animate(this.transform);
+	}
+		
+	void Start()
+	{
+//		mAnimate.ExpandContract(2.0f, 10, 2.0f, false, true, 0.0f);
+		mAnimate.Idle(2.5f, 5.0f);
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			mAnimate.StopIdle(true);
+		}
+	}
+}
