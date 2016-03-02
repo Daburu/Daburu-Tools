81d1a7d084eae5b63d3eb4e1c3016e54497312d0
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-03-02 23:52:36 +0800

    Implemented LocalMoveToAction

---
+++ DaburuTools/Assets/DaburuTools/Action/Leaves/LocalMoveToAction.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace DaburuTools
+{
+	namespace Action
+	{
+		public class LocalMoveToAction : Action
+		{
+			Transform mTransform;
+			Vector3 mvecInitialLocalPos;
+			Vector3 mvecDesiredLocalPos;
+			float mfActionDuration;
+			float mfElaspedDuration;
+
+			public LocalMoveToAction(Transform _transform)
+			{
+				mTransform = _transform;
+				SetupLocalMoveToAction();
+			}
+
+			public LocalMoveToAction(Transform _transform, Vector3 _desiredLocalPosition, float _actionDuration)
+			{
+				mTransform = _transform;
+				SetupLocalMoveToAction();
+				SetLocalMoveToAction(_desiredLocalPosition, _actionDuration);
+			}
+
+			public void SetLocalMoveToAction(Vector3 _desiredLocalPosition, float _actionDuration)
+			{
+				mvecDesiredLocalPos = _desiredLocalPosition;
+				mfActionDuration = _actionDuration;
+			}
+
+			private void SetupLocalMoveToAction()
+			{
+				mvecInitialLocalPos = mTransform.localPosition;
+				mfElaspedDuration = 0f;
+			}
+
+			public override void OnActionBegin()
+			{
+				base.OnActionBegin();
+
+				SetupLocalMoveToAction(); 
+			}
+
+			public override void RunAction()
+			{
+				base.RunAction();
+
+				mfElaspedDuration += Time.deltaTime;
+
+				float t = mfElaspedDuration / mfActionDuration;
+				mTransform.localPosition = Vector3.LerpUnclamped(mvecInitialLocalPos, mvecDesiredLocalPos, t);
+
+				// Remove self after action is finished.
+				if (mfElaspedDuration > mfActionDuration)
+				{
+					mTransform.localPosition = mvecDesiredLocalPos;	// Force it to be the exact scale that it wants.
+					OnActionEnd();
+					mParent.Remove(this);
+				}
+			}
+
+			public override bool Add(Action _Action)
+			{
+				return false;
+			}
+			public override bool Remove(Action _Action)
+			{
+				return false;
+			}
+			public override LinkedListNode<Action> GetListHead() { return null; }
+		}
+	}
+}
---
+++ DaburuTools/Assets/SandBox/Animate_Test_Child.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using DaburuTools.Action;
+
+public class Animate_Test_Child : MonoBehaviour {
+
+	ActionSequence mActionSequence;
+
+	void Awake()
+	{
+		
+	}
+
+	void Start()
+	{
+		LocalMoveToAction[] localMoveToActions = {
+			new LocalMoveToAction(this.transform, new Vector3(1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, -1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(-1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, 1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, -1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(-1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, 1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, -1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(-1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, 1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, -1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(-1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, 1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, -1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(-1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, 1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, -1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(-1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, 1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, -1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(-1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, 1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, -1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(-1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, 1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, -1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(-1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, 1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, -1.5f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(-1.5f, 0.0f, 0.0f), 0.25f),
+			new LocalMoveToAction(this.transform, new Vector3(0.0f, 0.0f, 1.5f), 0.25f)
+		};
+
+		ActionHandler.RunAction(new ActionSequence(localMoveToActions));
+	}
+
+	void Update()
+	{
+		
+	}
+}
