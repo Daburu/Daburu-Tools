8cbe71f930fab438578d008bd1b12274cc7c7abd
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-03-03 00:11:17 +0800

    Implemented RotateToAction

--- DaburuTools/Assets/SandBox/Animate_Test.cs
+++ DaburuTools/Assets/SandBox/Animate_Test.cs
@@ -17,22 +17,25 @@ public class Animate_Test : MonoBehaviour {
 
 	void Start()
 	{
-		ScaleToAction[] scaleActions = {
-			new ScaleToAction(this.transform, new Vector3(1.5f, 1.5f, 1.5f), 2.0f),
-			new ScaleToAction(this.transform, new Vector3(1.0f, 1.0f, 1.0f), 1.0f)
-		};
-		MoveToAction[] moveActions = {
-			new MoveToAction(this.transform, new Vector3(5.0f, 5.0f, 5.0f), 2.0f),
-			new MoveToAction(this.transform, new Vector3(-5.0f, -5.0f, -5.0f), 10.0f)
-		};
+//		ScaleToAction[] scaleActions = {
+//			new ScaleToAction(this.transform, new Vector3(1.5f, 1.5f, 1.5f), 2.0f),
+//			new ScaleToAction(this.transform, new Vector3(1.0f, 1.0f, 1.0f), 1.0f)
+//		};
+//		MoveToAction[] moveActions = {
+//			new MoveToAction(this.transform, new Vector3(5.0f, 5.0f, 5.0f), 2.0f),
+//			new MoveToAction(this.transform, new Vector3(-5.0f, -5.0f, -5.0f), 10.0f)
+//		};
+//
+//		ActionSequence scaleActionSequence = new ActionSequence(scaleActions);
+//		ActionSequence moveActionSequence = new ActionSequence(moveActions);
+//		ActionParallel actionParallel = new ActionParallel();
+//		actionParallel.Add(scaleActionSequence);
+//		actionParallel.Add(moveActionSequence);
+//
+//		ActionHandler.RunAction(actionParallel);
 
-		ActionSequence scaleActionSequence = new ActionSequence(scaleActions);
-		ActionSequence moveActionSequence = new ActionSequence(moveActions);
-		ActionParallel actionParallel = new ActionParallel();
-		actionParallel.Add(scaleActionSequence);
-		actionParallel.Add(moveActionSequence);
-
-		ActionHandler.RunAction(actionParallel);
+		RotateToAction rotateToAction = new RotateToAction(this.transform, new Vector3(45.0f, 45.0f, 45.0f), 10.0f);
+		ActionHandler.RunAction(rotateToAction);
 	}
 
 	void Update()
