cd653a038776b2133048231107221f6edc4e9be5
ColonDHappyface <colondhappyface@gmail.com>
2016-03-04 02:58:07 +0800

    Even More Features for Graph.cs!
    Graph.cs
    - Added more equations to help with the static initializers
    - Added Graph.Quadratic: Creates a U-shaped graph
    - Added Graph.InverseQuadratic: Creates a N-shaped graph
    - Added Graph.Dipper: Creates similar to an exponential graph, but with
    a dip at the start
    - Added Graph.Bobber: Creates similar to an inverse-exponential graph,
    but with a bob-up at the end
    - Added Graph.Multiply(): Returns the product of f(x) and g(x) at value
    x of both graphs

--- DaburuTools/Assets/DaburuTools/Graph/Graph.cs
+++ DaburuTools/Assets/DaburuTools/Graph/Graph.cs
@@ -99,6 +99,19 @@
             {
                 return (_graphA.ReadUnclamped(_x) + _graphB.ReadUnclamped(_x)) / 2f;
             }
+            
+            /// <summary>
+            /// Return the product of two graphs. Despite this function does not keep within range of 0 to 1, if both f(x)
+            /// and g(x) does not exceed 1, the product would stay within 1
+            /// </summary>
+            /// <param name="_graphA"> The first graph </param>
+            /// <param name="_graphB"> The second graph </param>
+            /// <param name="_x"> The x value of the graph </param>
+            /// <returns> Return the product of both graphs </returns>
+            public static float Multiply(Graph _graphA, Graph _graphB, float _x)
+            {
+                return _graphA.ReadUnclamped(_x) * _graphB.ReadUnclamped(_x);
+            }
 
         // Private Static Functions
             // KeepInRange(): Positivize all negatives x values. Mod all x that is more than 1
@@ -115,6 +128,10 @@
             private static float ExponentialEquation(float _x) { return _x * _x; }
             private static float InverseExponentialEquation(float _x) { return 1f - (ExponentialEquation(1f - _x)); }
             private static float SmoothStepEquation(float _x) { return _x * _x * (3f - 2f * _x); }
+            private static float QuadraticEquation(float _x) { return 4f * _x * (_x - 1f) + 1f; }
+            private static float InverseQuadraticEquation(float _x) { return 4f * _x * (1f - _x); }
+            private static float DipperEquation(float _x) { return (3f * _x * _x * _x - _x) / 2f; }
+            private static float BobberEquation(float _x) { return 1f - DipperEquation(1f - _x); }
 
         // Static Initializers
             /// <summary> { f(x) = x } Creates a linear graph equation, this is used to represent the 'default' of all Graph.cs types </summary>
@@ -123,9 +140,17 @@
             public static Graph InverseLinear { get { return new Graph(InverseLinearEquation); } }
             /// <summary> { f(x) = x^2 } Creates a exponential graph. Smooth start; Steep stop </summary>
             public static Graph Exponential { get { return new Graph(ExponentialEquation); } }
-            /// <summary> { f(x) = 1-((1-x)^2) } Creates an inverse exponential graph. Steep Start; Smooth stop </summary>
+            /// <summary> { f(x) = 1 - ((1 - x)^2) } Creates an inverse exponential graph. Steep Start; Smooth stop </summary>
             public static Graph InverseExponential { get { return new Graph(InverseExponentialEquation); } }
             /// <summary> { f(x) = 2x^3 - 2x^3 } Creates a slow start and end, with a steep body. Smooth Start; Smooth stop </summary>
             public static Graph SmoothStep { get { return new Graph(SmoothStepEquation); } }
+            /// <summary> { f(x) = 4x^2 - 4x } Creates a happy curve (U-shaped) </summary>
+            public static Graph Quadratic { get { return new Graph(QuadraticEquation); } }
+            /// <summary> { f(x) = 4x - 4x^2 } Creates a sad curve (N-shaped) </summary>
+            public static Graph InverseQuadratic { get { return new Graph(InverseQuadraticEquation); } }
+            /// <summary> { f(x) = (3x^3 - x) / 2 } Creates a similar curve to exponential, except it dips into the negatives at the front </summary>
+            public static Graph Dipper { get { return new Graph(DipperEquation); } }
+            /// <summary> { f(x) = (1 - (3(1 - x)^3 + x - 2)) / 2 } Create a similar curve to inverse-exponential, excepts it bobs above the graph at the end </summary>
+            public static Graph Bobber { get { return new Graph(BobberEquation); } }
     }
 }
