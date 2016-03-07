c321ec734fff376e805e7a62c70c6ab97ee2de30
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-03-07 15:09:41 +0800

    Implemented RotateByAction2D

---
+++ DaburuTools/Assets/DaburuTools/Action/Leaves/RotateByAction2D.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace DaburuTools
+{
+	namespace Action
+	{
+		public class RotateByAction2D : Action
+		{
+			Transform mTransform;
+			float mfAccumulatedZEulerAngle;
+			float mfDesiredTotalZEulerAngle;
+			Vector3 mvecDeltaPerSecond;
+			float mfActionDuration;
+			float mfElaspedDuration;
+
+			public RotateByAction2D(Transform _transform)
+			{
+				mTransform = _transform;
+				SetupMoveToAction();
+			}
+
+			public RotateByAction2D(Transform _transform, float _desiredZEulerAngle, float _actionDuration)
+			{
+				mTransform = _transform;
+				SetupMoveToAction();
+				SetMoveToAction(_desiredZEulerAngle, _actionDuration);
+			}
+
+			public void SetMoveToAction(float _desiredZEulerAngle, float _actionDuration)
+			{
+				mfDesiredTotalZEulerAngle = _desiredZEulerAngle;
+				mfActionDuration = _actionDuration;
+				mvecDeltaPerSecond = Vector3.forward * _desiredZEulerAngle / mfActionDuration;	// Cache so don't need to calcualte every RunAction.
+			}
+
+			private void SetupMoveToAction()
+			{
+				mfAccumulatedZEulerAngle = 0f;
+				mfElaspedDuration = 0f;
+			}
+
+			public override void RunAction()
+			{
+				base.RunAction();
+
+				// It is less tricky to track the action by elasped time.
+				// Otherwise, we need to check the sqrDist of both vec3s
+				// for when we need to terminate the action.
+				mfElaspedDuration += Time.deltaTime;
+
+				Vector3 delta = mvecDeltaPerSecond * Time.deltaTime;
+				mTransform.Rotate(delta);
+				mfAccumulatedZEulerAngle += delta.z;
+
+				// Remove self after action is finished.
+				if (mfElaspedDuration > mfActionDuration)
+				{
+					Vector3 imperfection = Vector3.forward * (mfDesiredTotalZEulerAngle - mfAccumulatedZEulerAngle);
+					mTransform.Rotate(imperfection);	// Force to exact delta displacement.
+
+					OnActionEnd();
+					mParent.Remove(this);
+				}
+			}
+
+			public override bool Add(Action _Action)
+			{
+				return false;
+			}
+			public override bool Remove(Action _Action)
+			{
+				return false;
+			}
+			public override LinkedListNode<Action> GetListHead() { return null; }
+		}
+	}
+}
