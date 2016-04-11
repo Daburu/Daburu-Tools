44e00ae3245eca61548c9832a40e2018d645f499
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-04-11 22:21:49 +0800

    Created GenericMenu Inspector.

--- DaburuTools/Assets/DaburuTools/UI/Generic Menu/GenericMenu.cs
+++ DaburuTools/Assets/DaburuTools/UI/Generic Menu/GenericMenu.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using System.Collections;
+using UnityEngine.UI;
 
-public class GenericMenu : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+namespace DaburuTools
+{
+	namespace UI
+	{
+		public class GenericMenu : MonoBehaviour
+		{
+			public string[] mArrStrButtonNames;
+			public Button.ButtonClickedEvent[] mArrButtonEvents;
+		}
 	}
 }
