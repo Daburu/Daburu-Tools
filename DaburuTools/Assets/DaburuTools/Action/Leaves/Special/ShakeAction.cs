69e10a885092fac9edeb95caa4ad87d8ecfdcab5
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-04-15 13:48:46 +0800

    Implemented ShakeAction

---
+++ DaburuTools/Assets/DaburuTools/Action/Leaves/Special/ShakeAction.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DaburuTools
+{
+	namespace Action
+	{
+		public class ShakeAction : Action
+		{
+			Transform mTransform;
+			int mnNumShakes;
+			float mfShakePeriod;
+			float mfShakeIntensity;
+			Graph mAttenuationGraph;
+
+			Vector3 mVecDeltaPos;
+			float mfElaspedDuration;
+			int mnCurrentCycle;
+
+			public ShakeAction(Transform _transform, int _numShakes, float _shakeIntensity)
+			{
+				mTransform = _transform;
+
+				SetNumShakes(_numShakes);
+				SetShakeIntensity(_shakeIntensity);
+				SetAttenuationGraph(Graph.One);
+
+				SetShakePeriod(0.05f);
+			}
+			public ShakeAction(Transform _transform, int _numShakes, float _shakeIntensity, Graph _attenuationGraph)
+			{
+				mTransform = _transform;
+
+				SetNumShakes(_numShakes);
+				SetShakeIntensity(_shakeIntensity);
+				SetAttenuationGraph(_attenuationGraph);
+
+				SetShakePeriod(0.05f);
+			}
+				
+			public void SetNumShakes(int _newNumShakes)
+			{
+				mnNumShakes = _newNumShakes;
+			}
+			public void SetShakeIntensity(float _newShakeIntensity)
+			{
+				mfShakeIntensity = _newShakeIntensity;
+			}
+			public void SetAttenuationGraph(Graph _newAttenuationGraph)
+			{
+				mAttenuationGraph = _newAttenuationGraph;
+			}
+			public void SetShakeFrequency(float _newShakeFrequency)
+			{
+				SetShakePeriod(1.0f / _newShakeFrequency);
+			}
+			public void SetShakeByDuration(float _newShakeDuration, int _newNumShakes)
+			{
+				SetNumShakes(_newNumShakes);
+				SetShakePeriod(_newShakeDuration / _newNumShakes);
+			}
+			private void SetShakePeriod(float _newShakePeriod)
+			{
+				mfShakePeriod = _newShakePeriod;
+			}
+			private void SetupAction()
+			{
+				mfElaspedDuration = 0f;
+				mnCurrentCycle = 0;
+				mVecDeltaPos = Vector3.zero;
+			}
+			protected override void OnActionBegin()
+			{
+				base.OnActionBegin();
+
+				SetupAction(); 
+			}
+
+
+			// Currently only expands then shrinks. Ending with shrink.
+			public override void RunAction()
+			{
+				base.RunAction();
+
+				if (mTransform == null)
+				{
+					// Debug.LogWarning("DaburuTools.Action: mTransform Deleted prematurely");
+					mParent.Remove(this);
+					return;
+				}
+
+				mfElaspedDuration += Time.deltaTime;
+				float mfCycleElasped = mfElaspedDuration - mfShakePeriod * mnCurrentCycle;
+				if (mfCycleElasped > mfShakePeriod)
+				{
+					mnCurrentCycle = (int) (mfElaspedDuration / mfShakePeriod);
+
+					// Remove self after action is finished.
+					if (mnCurrentCycle >= mnNumShakes)
+					{
+						// Force it back to original position.
+						mTransform.position -= mVecDeltaPos;
+
+						OnActionEnd();
+						mParent.Remove(this);
+					}
+					else
+					{
+						// Set back to original position.
+						mTransform.position -= mVecDeltaPos;
+						// Set new shake pos.
+						float t = mAttenuationGraph.Read(mfElaspedDuration / (mfShakePeriod * mnNumShakes));
+						mVecDeltaPos = Random.insideUnitSphere * mfShakeIntensity * t;
+						mTransform.position += mVecDeltaPos;
+					}
+				}
+			}
+			public override void MakeResettable(bool _bIsResettable)
+			{
+				base.MakeResettable(_bIsResettable);
+			}
+			public override void Reset()
+			{
+				SetupAction();
+			}
+		}
+	}
+
+}
---
+++ DaburuTools/Assets/SandBox/Action Tests/ShakeTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using DaburuTools.Action;
+using DaburuTools;
+
+public class ShakeTest : MonoBehaviour
+{
+	void Start()
+	{
+		Graph attenGraph = new Graph((float _x) =>
+			{
+				if (_x < 0.5f)
+					return _x * 2.0f;
+				else
+					return _x * -2.0f + 2.0f;
+			});
+		ShakeAction shake = new ShakeAction(transform, 20, 1.0f, attenGraph);
+//		shake.SetShakeFrequency(50.0f);
+		shake.SetShakeByDuration(10.0f, 1000);
+		shake.SetShakeIntensity(1.5f);
+		ActionHandler.RunAction(shake);
+	}
+	
+	void Update()
+	{
+	
+	}
+}
