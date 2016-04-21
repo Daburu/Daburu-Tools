5e9c7fe10f3b51515c992de167f738b9b5dac657
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-04-21 22:39:21 +0800

    Implemented VolumeToAction

---
+++ DaburuTools/Assets/DaburuTools/Action/Leaves/Miscellaneous/VolumeToAction.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace DaburuTools
+{
+	namespace Action
+	{
+		public class VolumeToAction : Action
+		{
+			AudioSource mAudioSource;
+			float mfDesiredVolume;
+			float mfActionDuration;
+			Graph mGraph;
+
+			float mfOriginalVolume;
+			float mfElaspedDuration;
+
+			public VolumeToAction(AudioSource _audioSource, float _desiredVolume, float _actionDuration)
+			{
+				mAudioSource = _audioSource;
+				SetDesiredVolume(_desiredVolume);
+				SetActionDuration(_actionDuration);
+				SetGraph(Graph.Linear);
+				SetupAction();
+			}
+
+			public void SetDesiredVolume(float _newDesiredVolume)
+			{
+				mfDesiredVolume = _newDesiredVolume;
+			}
+			public void SetActionDuration(float _newActionDuration)
+			{
+				mfActionDuration = _newActionDuration;
+			}
+			public void SetGraph(Graph _newGraph)
+			{
+				mGraph = _newGraph;
+			}
+			private void SetupAction()
+			{
+				mfOriginalVolume = mAudioSource.volume;
+				mfElaspedDuration = 0f;
+			}
+			protected override void OnActionBegin()
+			{
+				base.OnActionBegin();
+
+				SetupAction(); 
+			}
+
+
+
+			public override void RunAction()
+			{
+				base.RunAction();
+
+				mfElaspedDuration += ActionDeltaTime(mbIsUnscaledDeltaTime);
+
+				float t = mGraph.Read(mfElaspedDuration / mfActionDuration);
+				mAudioSource.volume = Mathf.Lerp(mfOriginalVolume, mfDesiredVolume, t);
+
+				// Remove self after action is finished.
+				if (mfElaspedDuration > mfActionDuration)
+				{
+					// Snap volume to desired volume.
+					mAudioSource.volume = mfDesiredVolume;
+
+					OnActionEnd();
+					mParent.Remove(this);
+				}
+			}
+			public override void MakeResettable(bool _bIsResettable)
+			{
+				base.MakeResettable(_bIsResettable);
+			}
+			public override void Reset()
+			{
+				SetupAction();
+			}
+		}
+	}
+}
---
+++ DaburuTools/Assets/SandBox/Action Tests/VolumeToActionTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using DaburuTools.Action;
+using DaburuTools;
+
+public class VolumeToActionTest : MonoBehaviour
+{
+	void Start()
+	{
+		VolumeToAction volDown = new VolumeToAction(gameObject.GetComponent<AudioSource>(), 0.0f, 5.0f);
+		volDown.SetGraph(Graph.Exponential);
+		VolumeToAction volUp = new VolumeToAction(gameObject.GetComponent<AudioSource>(), 1.0f, 5.0f);
+		volUp.SetGraph(Graph.InverseExponential);
+		ActionSequence sequence = new ActionSequence(volDown, volUp);
+		ActionHandler.RunAction(new ActionRepeatForever(sequence));
+	}
+}
