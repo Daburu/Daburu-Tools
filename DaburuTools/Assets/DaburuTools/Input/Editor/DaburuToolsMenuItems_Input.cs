bc0b9e095318b86b0b887959c40bce6ef2f2647c
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-04-26 15:40:31 +0800

    Implemented Input module.

---
+++ DaburuTools/Assets/DaburuTools/Input/Editor/DaburuToolsMenuItems_Input.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEditor;
+using DaburuTools.Input;
+
+public class DaburuToolsMenuItems_Input
+{
+	[MenuItem("DaburuTools/Input/Make Selected Camera Gyro Controlled", false, 1)]
+	private static void DT_Input_MakeCameraGyroControlled()
+	{
+		GameObject cameraGameObject = Selection.activeGameObject;
+
+		if (cameraGameObject.GetComponent<GyroscopeCamera>() != null)
+		{
+			Debug.LogWarning("Camera is already gyro controlled.");
+			return;
+		}
+
+		Undo.AddComponent(cameraGameObject, typeof(GyroscopeCamera));
+		Debug.Log("\"" +cameraGameObject.name + "\" is now controlled by gyroscope.");
+	}
+
+	[MenuItem("DaburuTools/Input/Make Main Camera Gyro Controlled", false, 2)]
+	private static void DT_Input_MakeMainCameraGyroControlled()
+	{
+		if (Camera.main == null)
+		{
+			Debug.LogWarning("No main camera found.");
+			return;
+		}
+
+		if (Camera.main.gameObject.GetComponent<GyroscopeCamera>() != null)
+		{
+			Debug.LogWarning("Main camera is already gyro controlled");
+			EditorGUIUtility.PingObject(Camera.main.gameObject);
+			Selection.activeObject = Camera.main.gameObject;
+			return;
+		}
+
+		Undo.AddComponent(Camera.main.gameObject, typeof(GyroscopeCamera));
+		Debug.Log("Main Camera: \"" + Camera.main.gameObject.name + "\" is now controlled by gyroscope.");
+	}
+
+	[MenuItem("DaburuTools/Input/Remove Gyro Controls from Selected Camera", false, 51)]
+	private static void DT_Input_RemoveGyroControlFromCamera()
+	{
+		GameObject cameraGameObject = Selection.activeGameObject;
+		GyroscopeCamera gyroCamScript = cameraGameObject.GetComponent<GyroscopeCamera>();
+
+		if (gyroCamScript == null)
+		{
+			Debug.LogWarning("Camera is already not controlled by gyroscope.");
+			return;
+		}
+
+		if ((int)gyroCamScript.mCameraPerspective == 1)	// If Third Person
+		{
+			if (gyroCamScript.camPivot == null)
+				Debug.LogWarning("CamPivot was not found. Could not complete cleanup. Did you move the third person cam pivot away?");
+			else
+			{
+				Undo.SetTransformParent(gyroCamScript.transform, gyroCamScript.camPivot.transform.parent, "De-parent GyroCam from camPivot");
+				Undo.DestroyObjectImmediate(gyroCamScript.camPivot);
+			}
+		}
+
+		Undo.DestroyObjectImmediate(cameraGameObject.GetComponent<GyroscopeCamera>());
+		Debug.Log("\"" + cameraGameObject.name + "\" has its gyroscope controls removed.");
+	}
+
+	#region Validation Functions
+	[MenuItem("DaburuTools/Input/Make Selected Camera Gyro Controlled", true)]
+	[MenuItem("DaburuTools/Input/Remove Gyro Controls from Selected Camera", true)]
+	private static bool SelectedGameObjectIsCameraValidation()
+	{
+		if (Selection.activeGameObject == null)
+			return false;
+
+		// Returns true when the selected GameObject has a camera component.
+		if (Selection.activeGameObject.GetComponent<Camera>() != null)
+			return true;
+		else
+			return false;
+	}
+	#endregion
+}
---
+++ DaburuTools/Assets/DaburuTools/Input/GyroscopeCamera.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DaburuTools
+{
+	namespace Input
+	{
+		public class GyroscopeCamera : MonoBehaviour
+		{
+			// Perspective
+			public enum CameraPerspective { FirstPerson, ThirdPerson };
+			public CameraPerspective mCameraPerspective = CameraPerspective.FirstPerson;
+			public float mfThirdPersonPivotOffset = 0.0f;
+
+			// Editor
+			public float mouseSensitivityX = 3.5f;
+			public float mouseSensitivityY = 3.5f;
+			public bool mbLockNHideCursor = false;
+
+			// Mobile
+			private GameObject camParent, camYRotator;
+			public GameObject camPivot;
+			private Gyroscope gyro;
+			private float yAngleOffset = 0.0f;
+
+			// Component/Variable Cache
+			private Transform cameraTransform;
+			private float cameraRotationX, cameraRotationY;
+
+			private void Awake()
+			{
+				// Editor
+				cameraTransform = Camera.main.transform;
+				cameraRotationX = cameraTransform.localEulerAngles.x + 90.0f;
+				cameraRotationY = cameraTransform.localEulerAngles.y;
+				if (mbLockNHideCursor)
+				{
+					Cursor.lockState = CursorLockMode.Locked;
+					Cursor.visible = false;
+				}
+
+				// Mobile
+				gyro = UnityEngine.Input.gyro;
+				gyro.enabled = true;
+
+				// Common
+				switch (mCameraPerspective)
+				{
+				case CameraPerspective.FirstPerson:
+					camPivot = new GameObject("CamPivot");
+					camPivot.transform.position = transform.position;
+					camPivot.transform.parent = transform.parent;
+					transform.parent = camPivot.transform;
+					break;
+				case CameraPerspective.ThirdPerson:
+					if (camPivot == null)
+						Debug.LogError("Missing CamPivot GameObject. Did you move it away? Try toggling the Camera Perspective field to re-create it at the right spot.");
+					else if (camPivot != transform.parent.gameObject)
+						Debug.LogError("Wrong CamPivot gameObject. Did you move it away? Try toggling the Camera Perspective field to re-create it at the right spot.");
+					break;
+				}
+
+				camParent = new GameObject ("CamParent");
+				camParent.transform.position = camPivot.transform.position;
+				camParent.transform.parent = camPivot.transform.parent;
+				camPivot.transform.parent = camParent.transform;
+
+				// Rotate the parent object by 90 degrees around the x axis
+				camParent.transform.Rotate(Vector3.right, 90);
+
+				// Set up the parent object of camParent to handle y rotation.
+				camYRotator = new GameObject("CamYRotator");
+				camYRotator.transform.position = transform.position;
+				camYRotator.transform.parent = camParent.transform.parent;
+				camParent.transform.parent = camYRotator.transform;
+			}
+
+			private void Update ()
+			{
+				// Editor
+				#if UNITY_EDITOR
+
+				cameraRotationX += UnityEngine.Input.GetAxis("Mouse Y") * mouseSensitivityY;
+				cameraRotationY += UnityEngine.Input.GetAxis("Mouse X") * mouseSensitivityX;
+
+				switch (mCameraPerspective)
+				{
+				case CameraPerspective.FirstPerson:
+					transform.localEulerAngles = new Vector3(-cameraRotationX, 0, 0);
+					camYRotator.transform.localEulerAngles = new Vector3(0, cameraRotationY, 0);
+					break;
+				case CameraPerspective.ThirdPerson:
+					camPivot.transform.localEulerAngles = new Vector3(-cameraRotationX, 0, 0);
+					camYRotator.transform.localEulerAngles = new Vector3(0, cameraRotationY, 0);
+					break;
+				}
+
+				//Mobile
+				#else
+				// Invert the z and w of the gyro attitude
+				Quaternion rotFix = new Quaternion(gyro.attitude.x, gyro.attitude.y, -gyro.attitude.z, -gyro.attitude.w);
+
+				// Now set the local rotation of the child camera object
+				switch (mCameraPerspective)
+				{
+				case CameraPerspective.FirstPerson:
+					transform.localRotation = rotFix;
+				case CameraPerspective.ThirdPerson:
+					camPivot.transform.localRotation = rotFix;
+				}
+				#endif
+			}
+
+			public void CenterCamera()
+			{
+				camYRotator.transform.Rotate(Vector3.up, -yAngleOffset);
+				yAngleOffset = -transform.rotation.eulerAngles.y;
+				camYRotator.transform.Rotate(Vector3.up, yAngleOffset);
+			}
+		}
+	}
+}
