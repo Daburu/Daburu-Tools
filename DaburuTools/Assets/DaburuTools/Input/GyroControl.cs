3cf090312659683b5ffa13367eb73a57bfb7e75e
Nyankai <niankaic@gmail.com>
2016-04-28 23:56:46 +0800

    GyroControl.cs - More reliable fix, X-axis is still being stubborn
    > GyroControl.cs
    - Added SnapTo enums, to be used with the class only
    - Fixed 'third-person' perspective, code uses Quaternion instead of
    Euler functions to prevent gimble-locking
    - Renamed certain variables to remain consistent
    - Renamed certain variables to be more understandable
    - BUG [UNSOLVED]: Code is fine when offsetting y-axis of the gameObject,
    but will mirror along x = 0 plane on the x-axis

--- DaburuTools/Assets/DaburuTools/Input/GyroControl.cs
+++ DaburuTools/Assets/DaburuTools/Input/GyroControl.cs
@@ -19,19 +19,22 @@ namespace DaburuTools
 			[Header("Pivot Properties")]
 			[Tooltip("The pivot of gyroscope rotation. If nothing is assigned , the pivot will be the center of the current transform")]
 			[SerializeField] private Transform m_RotationPivot = null;
+			[Tooltip("Determines if the pivot should rotate too. This will not affect if the pivot is the current object")]
+			[SerializeField] private bool bIsPivotRotating = false;
 
 			[Header("Reset Properties")]
-			[Tooltip("Determine what SetOffsetRotation() do. Worldaxis = snaps current rotation to world axis, InitialRotation = snaps current rotation to the initial rotation of the object")]
+			[Tooltip("Determine what SnapToPoint() do. Worldaxis = snaps current rotation to world axis, InitialRotation = snaps current rotation to the initial rotation of the object")]
 			[SerializeField] private SnapTo enum_snapTo = SnapTo.InitialRotation;
 
 			// Un-Editable Variables
 			private Gyroscope m_Gyroscope;				// m_Gyroscope: A reference to the gyroscope
 			private Quaternion m_gyroscopeRotation;		// m_gyroscopeRotation: The proper axis-defined rotation of the gyroscope
-			private Quaternion m_UnityWorldRotation;	// m_UnityWorldRotation: The current rotation that goes along with the world-axis
+			private Quaternion m_unityWorldRotation;	// m_unityWorldRotation: The current rotation that goes along with the world-axis
 
-			private Vector3 mVectorFromPivot;
-			private Quaternion m_initialRotationOnAwake;
-			private float mf_offsetRotation = 0f;
+			private Vector3 m_vectorFromPivot;						// m_vectorFromPivot: The distance between the this object and the pivot
+			private Quaternion m_initialRotationOnAwake;			// m_initialRotationOnAwake: The initial rotation of this object when this script is awake
+			private Quaternion m_inverseInitialRotationOnAwake;		// m_inverseInitialRotationOnAwake: The inverse of the initial rotation of this object
+			private float mf_snapToPointOffsetRotation = 0f;		// mf_snapToPointOffsetRotation: The 'center-to-screen' y-axis offset
 
 			// Private Functions
 			// Awake(): is called at the start of the program
@@ -45,9 +48,10 @@ namespace DaburuTools
 				if (m_RotationPivot == null)
 					m_RotationPivot = this.transform;
 				
-				// Initialsation
-				mVectorFromPivot = this.transform.position - m_RotationPivot.transform.position;
-				m_initialRotationOnAwake = m_RotationPivot.rotation;
+				// Initialisation
+				m_vectorFromPivot = this.transform.position - m_RotationPivot.transform.position;
+				m_initialRotationOnAwake = transform.rotation;
+				m_inverseInitialRotationOnAwake = Quaternion.Inverse(m_initialRotationOnAwake);
 				UpdateGyroscopeRotation();
 
 				// Editor
@@ -65,21 +69,26 @@ namespace DaburuTools
 			{
 				UpdateGyroscopeRotation();
 
-				// Editor
-				#if UNITY_EDITOR
+				// Editor - Mouse Emulation
+#if UNITY_EDITOR
 				cameraRotationX += UnityEngine.Input.GetAxis("Mouse Y") * mouseSensitivityY;
 				cameraRotationY += UnityEngine.Input.GetAxis("Mouse X") * mouseSensitivityX;
 
 				m_gyroscopeRotation = Quaternion.Euler(-cameraRotationX, cameraRotationY, 0.0f);
-				#endif
+#endif
+				// Converts gyroscope rotation to unity world rotation
+				m_unityWorldRotation = Quaternion.Euler(0f, mf_snapToPointOffsetRotation, 0f) * m_gyroscopeRotation;
+				transform.rotation = m_unityWorldRotation;
 
-				m_UnityWorldRotation = Quaternion.Euler(0f, mf_offsetRotation, 0f) * m_gyroscopeRotation;
+				// if: The current rotation pivot is not this object a.k.a Third Person Mode
+				if (m_RotationPivot != this.transform)
+				{
+					if (bIsPivotRotating)
+						m_RotationPivot.rotation = m_unityWorldRotation;
 
-				m_RotationPivot.rotation = m_UnityWorldRotation;
-				transform.position = (m_UnityWorldRotation * mVectorFromPivot) + m_RotationPivot.position;
-				transform.rotation = m_UnityWorldRotation;
+					transform.position = (m_unityWorldRotation * m_inverseInitialRotationOnAwake * m_vectorFromPivot) + m_RotationPivot.position;
+				}
 			}
-
 
 			// UpdateGyroscopeRotation(): Recalculates the gyroscope rotation and updates it into m_gyroscopeRotation;
 			private void UpdateGyroscopeRotation()
@@ -91,15 +100,15 @@ namespace DaburuTools
 			/// <summary>
 			/// Sets the current rotation of the gyroscope to be the initial roation of the object
 			/// </summary>
-			public void SetOffsetRotation()
+			public void SnapToPoint()
 			{
 				switch(enum_snapTo)
 				{
 					case SnapTo.InitialRotation:
-						mf_offsetRotation = m_initialRotationOnAwake.eulerAngles.y - m_gyroscopeRotation.eulerAngles.y;
+						mf_snapToPointOffsetRotation = m_initialRotationOnAwake.eulerAngles.y - m_gyroscopeRotation.eulerAngles.y;
 						break;
 					case SnapTo.WorldAxis:
-						mf_offsetRotation = -m_gyroscopeRotation.eulerAngles.y;
+						mf_snapToPointOffsetRotation = -m_gyroscopeRotation.eulerAngles.y;
 						break;
 				}
 			}
@@ -113,7 +122,7 @@ namespace DaburuTools
 			/// <summary>
 			/// Returns the rotation in relating to world axis
 			/// </summary>
-			public Quaternion WorldRotation { get { return m_gyroscopeRotation; } }
+			public Quaternion WorldRotation { get { return m_unityWorldRotation; } }
 		}
 	}
 }
