a8961a21f47b0058699a2b040840df3a0d45b898
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-07-06 22:33:25 +0800

    Created CanvasGroupAlphaToAction
    - Tested in Sandbox.

---
+++ DaburuTools/Assets/DaburuTools/Action/Leaves/Miscellaneous/CanvasGroupAlphaToAction.cs
@@ -0,0 +1,110 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace DaburuTools
+{
+	namespace Action
+	{
+		public class CanvasGroupAlphaToAction : Action
+		{
+			CanvasGroup mCanvasGroup;
+			float mfDesiredAlpha;
+			float mfActionDuration;
+			Graph mGraph;
+
+			float mfOriginalAlpha;
+			float mfElaspedDuration;
+
+			public CanvasGroupAlphaToAction(CanvasGroup _canvasGroup, Graph _graph, float _desiredAlpha, float _actionDuration)
+			{
+				mCanvasGroup = _canvasGroup;
+				SetGraph(_graph);
+				SetDesiredAlpha(_desiredAlpha);
+				SetActionDuration(_actionDuration);
+				SetupAction();
+			}
+			public CanvasGroupAlphaToAction(CanvasGroup _canvasGroup, float _desiredAlpha, float _actionDuration)
+			{
+				mCanvasGroup = _canvasGroup;
+				SetGraph(Graph.Linear);
+				SetDesiredAlpha(_desiredAlpha);
+				SetActionDuration(_actionDuration);
+				SetupAction();
+			}
+			public void SetDesiredAlpha(float _newDesiredAlpha)
+			{
+				mfDesiredAlpha = _newDesiredAlpha;
+			}
+			public void SetActionDuration(float _newActionDuration)
+			{
+				mfActionDuration = _newActionDuration;
+			}
+			public void SetGraph(Graph _newGraph)
+			{
+				mGraph = _newGraph;
+			}
+			private void SetupAction()
+			{
+				mfOriginalAlpha = mCanvasGroup.alpha;
+				mfElaspedDuration = 0f;
+			}
+			protected override void OnActionBegin()
+			{
+				base.OnActionBegin();
+
+				SetupAction(); 
+			}
+
+
+
+			public override void RunAction()
+			{
+				base.RunAction();
+
+				mfElaspedDuration += ActionDeltaTime(mbIsUnscaledDeltaTime);
+
+				float t = mGraph.Read(mfElaspedDuration / mfActionDuration);
+				mCanvasGroup.alpha = mGraph.Read(Mathf.Lerp(mfOriginalAlpha, mfDesiredAlpha, t));
+
+				// Remove self after action is finished.
+				if (mfElaspedDuration > mfActionDuration)
+				{
+					// Snap to desired alpha.
+					mCanvasGroup.alpha = mfDesiredAlpha;
+
+					OnActionEnd();
+					mParent.Remove(this);
+				}
+			}
+			public override void MakeResettable(bool _bIsResettable)
+			{
+				base.MakeResettable(_bIsResettable);
+			}
+			public override void Reset()
+			{
+				SetupAction();
+			}
+			public override void StopAction(bool _bSnapToDesired)
+			{
+				if (!mbIsRunning)
+					return;
+
+				// Prevent it from Resetting.
+				MakeResettable(false);
+
+				// Simulate the action has ended. Does not really matter by how much.
+				mfElaspedDuration = mfActionDuration;
+
+				if (_bSnapToDesired)
+				{
+					// Snap to desired alpha.
+					mCanvasGroup.alpha = mfDesiredAlpha;
+				}
+
+				OnActionEnd();
+				mParent.Remove(this);
+			}
+		}
+	}
+}
--- DaburuTools/Assets/SandBox/Action Tests/UIActionTests.cs
+++ DaburuTools/Assets/SandBox/Action Tests/UIActionTests.cs
@@ -9,12 +9,14 @@ public class UIActionTests : MonoBehaviour
 	private Image mImage;
 	private RawImage mRawImage;
 	private Text mText;
+	private CanvasGroup mCanvasGroup;
 
 	void Awake()
 	{
 		mImage = transform.FindChild("ImageTest").GetComponent<Image>();
 		mRawImage = transform.FindChild("RawImageTest").GetComponent<RawImage>();
 		mText = transform.FindChild("TextTest").GetComponent<Text>();
+		mCanvasGroup = transform.FindChild("CanvasGroupTest").GetComponent<CanvasGroup>();
 
 		ImageAlphaToAction fadeImage = new ImageAlphaToAction(mImage, Graph.InverseExponential, 0.0f, 5.0f);
 		ActionHandler.RunAction(fadeImage);
@@ -24,5 +26,8 @@ public class UIActionTests : MonoBehaviour
 
 		TextAlphaToAction fadeText = new TextAlphaToAction(mText, 0.0f, 2.5f);
 		ActionHandler.RunAction(fadeText);
+
+		CanvasGroupAlphaToAction canvasGroupTest = new CanvasGroupAlphaToAction(mCanvasGroup, 0.0f, 1.0f);
+		ActionHandler.RunAction(canvasGroupTest);
 	}
 }
