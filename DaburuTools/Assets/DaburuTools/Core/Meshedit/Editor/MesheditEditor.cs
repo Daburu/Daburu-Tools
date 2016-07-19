46ce73cfddb830e2909d8dd1f6158658969ca229
Nyankai <niankaic@gmail.com>
2016-07-19 09:38:59 +0800

    Creation of Meshedit - An Inspector Mesh Editor
    > MesheditEditor.cs
    - CREATED an editor for mesh-edit, nothing is implemented
    - CREATED an property drawer for mesh-edit step
    
    > Meshedit.cs
    - CREATED a script for Meshedit
    - CREATED a script for MesheditStep struct

---
+++ DaburuTools/Assets/DaburuTools/Core/Meshedit/Editor/MesheditEditor.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MesheditEditor : Editor 
+{
+	// Serialised Properties
+
+	void OnEnable()
+	{
+		
+	}
+
+	public override void OnInspectorGUI ()
+	{
+		serializedObject.Update ();
+
+		serializedObject.ApplyModifiedProperties();
+	}
+}
+
+[CustomPropertyDrawer(typeof(MesheditStep))]
+public class MesheditStepDrawer : PropertyDrawer
+{
+	public override void OnGUI (Rect _rectPosition, SerializedProperty _serialisedProperty, GUIContent _GUIContLabel)
+	{
+		_GUIContLabel = EditorGUI.BeginProperty(_rectPosition, _GUIContLabel, _serialisedProperty);
+		Rect rectDrawer = EditorGUI.PrefixLabel (_rectPosition, GUIContent.none);
+		float fInspectorLength = rectDrawer.width;
+
+		rectDrawer.width *= 0.4f;
+		EditorGUI.PropertyField(rectDrawer, _serialisedProperty.FindPropertyRelative("m_enumMesheditStep"), GUIContent.none);
+
+		rectDrawer.x += rectDrawer.width;
+		rectDrawer.width = fInspectorLength;
+		rectDrawer.width *= 0.2f;
+		EditorGUIUtility.labelWidth = 15f;
+		EditorGUI.PropertyField(rectDrawer, _serialisedProperty.FindPropertyRelative("m_fVariableX"), new GUIContent("X"));
+		rectDrawer.x += rectDrawer.width;
+		EditorGUI.PropertyField(rectDrawer, _serialisedProperty.FindPropertyRelative("m_fVariableY"), new GUIContent("Y"));
+		rectDrawer.x += rectDrawer.width;
+		EditorGUI.PropertyField(rectDrawer, _serialisedProperty.FindPropertyRelative("m_fVariableZ"), new GUIContent("Z"));
+		
+		EditorGUI.EndProperty();
+	}	
+}
---
+++ DaburuTools/Assets/DaburuTools/Core/Meshedit/Meshedit.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+// Meshedit.cs: Edit the mesh before going into play mode!
+public class Meshedit : MonoBehaviour 
+{
+	// Serialised Variables
+	public MesheditStep[] marr_mesheditStep = null;
+
+	// Un-serialised Variables
+
+	// Private Functions
+	// Awake(): is called when the script is first initialised
+	void Awake () 
+	{
+		
+	}
+
+	// Update(): is called once per frame
+	void Update()
+	{
+		if (Application.isPlaying) 
+		{
+			Debug.Log (name + ".MeshEdit(Component): You left this script in play mode! This script is designed to be only executed in edit mode! Disabling itself!");
+			this.enabled = false;
+		}
+	}
+}
+
+[System.Serializable]
+// MesheditStep.cs: The data to show the steps to take 
+public struct MesheditStep
+{
+	public enum EnumMesheditStep { Position, EulerRotation, Scale };
+
+	public EnumMesheditStep m_enumMesheditStep;
+	public float m_fVariableX;
+	public float m_fVariableY;
+	public float m_fVariableZ;
+
+	public MesheditStep(EnumMesheditStep _enumMesheditStep, float _x, float _y, float _z)
+	{
+		m_enumMesheditStep = _enumMesheditStep;
+		m_fVariableX = _x;
+		m_fVariableY = _y;
+		m_fVariableZ = _z;
+	}
+}
