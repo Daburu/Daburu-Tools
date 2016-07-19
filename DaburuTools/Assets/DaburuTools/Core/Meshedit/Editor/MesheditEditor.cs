1710b9b7aa026b44a20c341ac95cd82c4f8a96ad
Nyankai <niankaic@gmail.com>
2016-07-19 16:23:53 +0800

    Meshedit v1.0 is out now!
    > MesheditEditor.cs:
    - CREATED more editor handling stuff
    
    > Meshedit.cs
    - CREATED a displaying system for the output model
    - CREATED the calculation algorithm to transform the mesh

--- DaburuTools/Assets/DaburuTools/Core/Meshedit/Editor/MesheditEditor.cs
+++ DaburuTools/Assets/DaburuTools/Core/Meshedit/Editor/MesheditEditor.cs
@@ -3,23 +3,37 @@ using UnityEditor;
 using UnityEditorInternal;
 
 [CustomEditor(typeof(Meshedit))]
+// MesheditEditor.cs: The editor of Mesh-Edit
 public class MesheditEditor : Editor 
 {
 	// Serialised Properties
 	private SerializedProperty mSPropList_mesheditStep = null;
+	private SerializedProperty mSProp_enumPivotType = null;
+
+	private SerializedProperty mSProp_bIsDisplayOutput = null;
+
+	// GUIContent Variables
+	private GUIContent mGUICont_bIsDisplayOutput
+		= new GUIContent ("Display Output", "Determine if it should display mesh output in the scene");
+	private GUIContent mGUICont_enumPivotType
+		= new GUIContent("Display At", "Determine the position at which the output model will display at");
 
 	// Private Variables
 	private ReorderableList mROList_mesheditStep = null;
 
 	void OnEnable()
 	{
+		// Serialisation Initialisation
 		mSPropList_mesheditStep = serializedObject.FindProperty ("mList_mesheditStep");
+		mSProp_enumPivotType = serializedObject.FindProperty ("m_enumPivotType");
 
+		mSProp_bIsDisplayOutput = serializedObject.FindProperty ("m_bIsDisplayOutput");
+
+		// Re-orderable List Settings
 		mROList_mesheditStep = new ReorderableList (serializedObject, mSPropList_mesheditStep, true, true, true, true);
-
 		mROList_mesheditStep.drawHeaderCallback = (Rect _rectROListHeader) => 
 		{
-			EditorGUI.LabelField(_rectROListHeader, new GUIContent("Order of Actions", "The order of action to be executed in sequence"));
+			EditorGUI.LabelField(_rectROListHeader, new GUIContent("Order of Transformation", "The order of transformation to be executed in sequence"));
 		};
 		mROList_mesheditStep.drawElementCallback = (Rect _rectROList, int _nIndex, bool _bIsActive, bool _bIsFocused) => 
 		{
@@ -38,18 +52,48 @@ public class MesheditEditor : Editor
 	public override void OnInspectorGUI ()
 	{
 		serializedObject.Update ();
-
 		EditorGUILayout.Space ();
+
+		// Transformation Propeties
+		EditorGUILayout.LabelField ("Transformation Properties", EditorStyles.boldLabel);
 		mROList_mesheditStep.DoLayoutList ();
+		EditorGUILayout.HelpBox ("Use the list above to transform the current mesh", MessageType.Info);
+		EditorGUILayout.Space ();
+
+		// Debugging Properties
+		EditorGUILayout.LabelField ("Debugging Properties", EditorStyles.boldLabel);
+		EditorGUILayout.PropertyField (mSProp_bIsDisplayOutput, mGUICont_bIsDisplayOutput);
+		if (mSProp_bIsDisplayOutput.boolValue) 
+		{
+			EditorGUI.indentLevel++;
+				EditorGUILayout.PropertyField (mSProp_enumPivotType, mGUICont_enumPivotType);
+				EditorGUILayout.HelpBox((mSProp_enumPivotType.enumValueIndex == 0 ? 
+					"The output model will be previewed with the gameObject's pivot" : "The output model will the world's origin as the pivot.") 
+					+ "\n\nNOTE: This will not affect the final output at all!", MessageType.Info);
+			EditorGUI.indentLevel--;
+		}
+		EditorGUILayout.Space ();
+
+		// Commission Properties
+		EditorGUILayout.LabelField ("Commission Properties", EditorStyles.boldLabel);
+		GUI.color = new Color (1f, 0.25f, 0.25f);
+		if (GUILayout.Button ("Commit", GUILayout.MinHeight (40f)))
+		{
+			thisMeshedit.Commit ();
+			return;
+		}
+		GUI.color = Color.white;
+		EditorGUILayout.HelpBox ("Click the button above to commit the changes to the gameObject. Once done, this script will be deleted", MessageType.Info);
 
 		serializedObject.ApplyModifiedProperties();
 	}
 
 	// Getter-Setter Functions
-	private GameObject RefGameObject { get { return ((Meshedit)serializedObject.targetObject).gameObject; } }
+	private Meshedit thisMeshedit { get { return (Meshedit)serializedObject.targetObject; } }
 }
 
 [CustomPropertyDrawer(typeof(MesheditStep))]
+// MesheditDrawer.cs: The property drawer for Mesh-Edit Steps
 public class MesheditStepDrawer : PropertyDrawer
 {
 	public override void OnGUI (Rect _rectPosition, SerializedProperty _serialisedProperty, GUIContent _GUIContLabel)
--- DaburuTools/Assets/DaburuTools/Core/Meshedit/Meshedit.cs
+++ DaburuTools/Assets/DaburuTools/Core/Meshedit/Meshedit.cs
@@ -1,20 +1,54 @@
 ﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 
 [ExecuteInEditMode]
 // Meshedit.cs: Edit the mesh before going into play mode!
 public class Meshedit : MonoBehaviour 
 {
+	public enum EnumPivotType { Position, Origin };
+
 	// Serialised Variables
 	[SerializeField] private List<MesheditStep> mList_mesheditStep = new List<MesheditStep>();
+	[SerializeField] private EnumPivotType m_enumPivotType = EnumPivotType.Origin;
+
+	[SerializeField] private bool m_bIsDisplayOutput = false;
 
 	// Un-serialised Variables
-
+	private Mesh m_mesh = null;
+	private Mesh m_meshOutput = null;
+	
 	// Private Functions
+	void OnDrawGizmosSelected()
+	{
+		if (m_bIsDisplayOutput) 
+		{
+			CalculateOutputAndUpdate ();
+
+			if (m_meshOutput != null)
+			{
+				Vector3 vec3Origin = m_enumPivotType == EnumPivotType.Origin ? Vector3.zero : transform.position;
+				for (int i = 0; i < m_meshOutput.triangles.Length / 3; i++)
+				{
+					Gizmos.DrawLine (vec3Origin + m_meshOutput.vertices[m_meshOutput.triangles[i * 3]], vec3Origin + m_meshOutput.vertices[m_meshOutput.triangles[i * 3 + 1]]);
+					Gizmos.DrawLine (vec3Origin + m_meshOutput.vertices[m_meshOutput.triangles[i * 3 + 1]], vec3Origin + m_meshOutput.vertices[m_meshOutput.triangles[i * 3 + 2]]);
+					Gizmos.DrawLine (vec3Origin + m_meshOutput.vertices[m_meshOutput.triangles[i * 3+ 2]], vec3Origin + m_meshOutput.vertices[m_meshOutput.triangles[i * 3]]);
+				}
+			}
+		}
+	}
+
 	// Awake(): is called when the script is first initialised
 	void Awake () 
 	{
-		
+		if (!Application.isPlaying) 
+		{
+			if (GetComponent<MeshFilter> () == null) 
+			{
+				Debug.LogWarning (name + ".Meshedit: No MeshFilter detected on gameObject!", this.gameObject);
+				return;
+			}
+		}
 	}
 
 	// Update(): is called once per frame
@@ -22,9 +56,83 @@ public class Meshedit : MonoBehaviour
 	{
 		if (Application.isPlaying) 
 		{
-			Debug.Log (name + ".MeshEdit(Component): You left this script in play mode! This script is designed to be only executed in edit mode! Disabling itself!");
+			Debug.LogWarning (name + ".Meshedit: You left this script in play mode! This script is designed to be only executed in edit mode! Disabling itself!", this.gameObject);
 			this.enabled = false;
 		}
+	}
+
+	// Public Functions
+	/// <summary>
+	/// Calculates the final model after it runs through the edit steps
+	/// </summary>
+	/// <returns> Returns the calculated mesh </returns>
+	public Mesh CalculateOutput()
+	{
+		// if: There are no items in the list
+		if (mList_mesheditStep.Count == 0)
+			return null;
+
+		m_mesh = GetComponent<MeshFilter> ().sharedMesh;
+
+		Mesh meshOutput = new Mesh ();
+		meshOutput.vertices = new Vector3[m_mesh.vertexCount];
+		meshOutput.triangles = m_mesh.triangles;
+		meshOutput.normals = m_mesh.normals;
+		meshOutput.uv = m_mesh.uv;
+		meshOutput.uv2 = m_mesh.uv2;
+		meshOutput.uv3 = m_mesh.uv3;
+		meshOutput.uv4 = m_mesh.uv4;
+
+		// for: Every mesh-edit step...
+		Vector3[] arr_vec3vertices = m_mesh.vertices;
+		for (int i = 0; i < mList_mesheditStep.Count; i++) 
+		{
+			// for: Every vertex in the existing mesh...
+			for (int j = 0; j < m_mesh.vertexCount; j++) 
+			{
+				switch (mList_mesheditStep [i].stepType) 
+				{
+				case MesheditStep.EnumMesheditStep.Position:
+					arr_vec3vertices [j] += mList_mesheditStep [i].vector3Value;
+					break;
+				case MesheditStep.EnumMesheditStep.EulerRotation:
+					arr_vec3vertices [j] = Quaternion.Euler (mList_mesheditStep [i].vector3Value) * arr_vec3vertices [j];
+					break;
+				default:
+					arr_vec3vertices [j] = new Vector3(
+						arr_vec3vertices[j].x * mList_mesheditStep[i].vector3Value.x,
+						arr_vec3vertices[j].y * mList_mesheditStep[i].vector3Value.y,
+						arr_vec3vertices[j].z * mList_mesheditStep[i].vector3Value.z);
+					break;
+				}
+			}
+		}
+		meshOutput.vertices = arr_vec3vertices;
+
+		return meshOutput;
+	}
+
+	/// <summary>
+	/// Calculates the final model and updates to class' new mesh. This will not override the existing mesh!
+	/// </summary>
+	public void CalculateOutputAndUpdate()
+	{
+		m_meshOutput = CalculateOutput ();
+	}
+
+	/// <summary>
+	/// Commit the changes to existing mesh
+	/// </summary>
+	public void Commit()
+	{
+		if (m_meshOutput == null)
+		{
+			Debug.LogWarning (name + ".Meshedit.Commit(): Trying to commit to an empty mesh! Reverting changes!");
+			return;
+		}
+		GetComponent<MeshFilter> ().mesh = m_meshOutput;
+		Debug.Log (name + ".Meshedit.Commit(): Commited the mesh of the gameObject. Removing this script from gameObject");
+		DestroyImmediate (this);
 	}
 }
 
@@ -34,10 +142,10 @@ public struct MesheditStep
 {
 	public enum EnumMesheditStep { Position, EulerRotation, Scale };
 
-	public EnumMesheditStep m_enumMesheditStep;
-	public float m_fVariableX;
-	public float m_fVariableY;
-	public float m_fVariableZ;
+	[SerializeField] private EnumMesheditStep m_enumMesheditStep;
+	[SerializeField] private float m_fVariableX;
+	[SerializeField] private float m_fVariableY;
+	[SerializeField] private float m_fVariableZ;
 
 	public MesheditStep(EnumMesheditStep _enumMesheditStep, float _x, float _y, float _z)
 	{
@@ -46,4 +154,31 @@ public struct MesheditStep
 		m_fVariableY = _y;
 		m_fVariableZ = _z;
 	}
+
+	// Getter-Setter Functions
+	/// <summary>
+	/// Gets or sets the type of the step.
+	/// </summary>
+	/// <value>The type of the step.</value>
+	public EnumMesheditStep stepType { get { return m_enumMesheditStep; } set { m_enumMesheditStep = value; } }
+
+	/// <summary>
+	/// Gets or sets the x value.
+	/// </summary>
+	/// <value>The x value.</value>
+	public float xValue { get { return m_fVariableX; } set { m_fVariableX = value; } }
+
+	/// <summary>
+	/// Gets or sets the y value.
+	/// </summary>
+	/// <value>The y value.</value>
+	public float yValue { get { return m_fVariableY; } set { m_fVariableY = value; } }
+
+	/// <summary>
+	/// Gets or sets the z value.
+	/// </summary>
+	/// <value>The z value.</value>
+	public float zValue { get { return m_fVariableZ; } set { m_fVariableZ = value; } }
+
+	public Vector3 vector3Value { get { return new Vector3 (m_fVariableX, m_fVariableY, m_fVariableZ); } }
 }
