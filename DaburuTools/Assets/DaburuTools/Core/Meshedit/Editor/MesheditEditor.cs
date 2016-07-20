cee8879c1dcff6dc436aa8c2f37d6771d793c6e0
Nyankai <niankaic@gmail.com>
2016-07-20 09:23:47 +0800

    Meshedit v1.2 - More stable version and exports!
    > MesheditEditor.cs
    - CREATED editor for Meshedit.cs
    
    > Meshedit.cs
    - CHANGED preview rendering method - much faster now
    - ADDED disable warning for editor scripting variables
    - ADDED exporting functions - export to the 'Assets' folder currently

--- DaburuTools/Assets/DaburuTools/Core/Meshedit/Editor/MesheditEditor.cs
+++ DaburuTools/Assets/DaburuTools/Core/Meshedit/Editor/MesheditEditor.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using UnityEditorInternal;
+using System.Collections.Generic;
 
 [CustomEditor(typeof(Meshedit))]
 // MesheditEditor.cs: The editor of Mesh-Edit
@@ -10,13 +11,28 @@ public class MesheditEditor : Editor
 	private SerializedProperty mSPropList_mesheditStep = null;
 	private SerializedProperty mSProp_enumPivotType = null;
 
+	private SerializedProperty mSProp_strMeshName = null;
+	//private SerializedProperty mSProp_strMeshPath = null;
+
 	private SerializedProperty mSProp_bIsDisplayOutput = null;
+	private SerializedProperty mSProp_bIsDisplayPivot = null;
+	private SerializedProperty mSProp_bIsExportMesh = null;
 
 	// GUIContent Variables
 	private GUIContent mGUICont_bIsDisplayOutput
-		= new GUIContent ("Display Output", "Determine if it should display mesh output in the scene");
+		= new GUIContent ("Display Preview?", "Determine if it should display output model in the scene");
 	private GUIContent mGUICont_enumPivotType
-		= new GUIContent("Display At", "Determine the position at which the output model will display at");
+		= new GUIContent("Display at", "Determine the position at which the output model will display at");
+
+	private GUIContent mGUICont_strMeshName 
+		= new GUIContent ("Mesh Name", "The name of the mesh");
+	private GUIContent mGUICont_strmeshPath
+		= new GUIContent ("Export To", "The directory of the saved mesh");
+
+	private GUIContent mGUICont_bIsDisplayPivot
+		= new GUIContent ("Display Pivot?", "Determine if it should display mesh pivot in the scene");
+	private GUIContent mGUICont_bIsExportMesh
+		= new GUIContent ("Export Mesh?", "Determine if the mesh should be exported on transformation");
 
 	// Private Variables
 	private ReorderableList mROList_mesheditStep = null;
@@ -27,7 +43,12 @@ public class MesheditEditor : Editor
 		mSPropList_mesheditStep = serializedObject.FindProperty ("mList_mesheditStep");
 		mSProp_enumPivotType = serializedObject.FindProperty ("m_enumPivotType");
 
+		mSProp_strMeshName = serializedObject.FindProperty ("m_strMeshName");
+		//mSProp_strMeshPath = serializedObject.FindProperty ("m_strMeshPath");
+
 		mSProp_bIsDisplayOutput = serializedObject.FindProperty ("m_bIsDisplayOutput");
+		mSProp_bIsDisplayPivot = serializedObject.FindProperty ("m_bIsDisplayPivot");
+		mSProp_bIsExportMesh = serializedObject.FindProperty ("m_bIsExportMesh");
 
 		// Re-orderable List Settings
 		mROList_mesheditStep = new ReorderableList (serializedObject, mSPropList_mesheditStep, true, true, true, true);
@@ -46,6 +67,7 @@ public class MesheditEditor : Editor
 				new Rect(_rectROList.x, _rectROList.y, _rectROList.width * 0.9f, EditorGUIUtility.singleLineHeight),
 				sPropElement, 
 				GUIContent.none);
+			thisMeshedit.CalculateOutputAndUpdate ();
 		};
 	}
 
@@ -56,34 +78,68 @@ public class MesheditEditor : Editor
 
 		// Transformation Propeties
 		EditorGUILayout.LabelField ("Transformation Properties", EditorStyles.boldLabel);
-		mROList_mesheditStep.DoLayoutList ();
-		EditorGUILayout.HelpBox ("Use the list above to transform the current mesh", MessageType.Info);
+		EditorGUILayout.HelpBox ("Use the list below to transform the current mesh", MessageType.Info);
+			mROList_mesheditStep.DoLayoutList ();
+
 		EditorGUILayout.Space ();
 
 		// Debugging Properties
 		EditorGUILayout.LabelField ("Debugging Properties", EditorStyles.boldLabel);
+		EditorGUI.BeginChangeCheck ();
 		EditorGUILayout.PropertyField (mSProp_bIsDisplayOutput, mGUICont_bIsDisplayOutput);
+		// if: There is a change detected in the boolean field
+		if (EditorGUI.EndChangeCheck ())
+			thisMeshedit.CalculateOutputAndUpdate ();	
 		if (mSProp_bIsDisplayOutput.boolValue) 
 		{
 			EditorGUI.indentLevel++;
+			EditorGUILayout.HelpBox((mSProp_enumPivotType.enumValueIndex == 0 ? 
+				"The output model will be previewed with the gameObject's pivot" : "The output model will the world's origin as the pivot.") 
+				+ "\n\nNOTE: This will not affect the final output at all!", MessageType.Info);
 				EditorGUILayout.PropertyField (mSProp_enumPivotType, mGUICont_enumPivotType);
-				EditorGUILayout.HelpBox((mSProp_enumPivotType.enumValueIndex == 0 ? 
-					"The output model will be previewed with the gameObject's pivot" : "The output model will the world's origin as the pivot.") 
-					+ "\n\nNOTE: This will not affect the final output at all!", MessageType.Info);
+			EditorGUILayout.PropertyField (mSProp_bIsDisplayPivot, mGUICont_bIsDisplayPivot);
+			EditorGUI.indentLevel--;
+		}
+		EditorGUILayout.Space ();
+
+		// Exporting Properties
+		EditorGUILayout.LabelField ("Exporting Properties", EditorStyles.boldLabel);
+		EditorGUILayout.PropertyField (mSProp_bIsExportMesh, mGUICont_bIsExportMesh);
+		if (mSProp_bIsExportMesh.boolValue)
+		{
+			EditorGUI.indentLevel++;
+			EditorGUILayout.PropertyField (mSProp_strMeshName, mGUICont_strMeshName);
+			//EditorGUILayout.LabelField (mGUICont_strmeshPath);
+			GUILayout.BeginHorizontal ();
+			EditorGUI.indentLevel++;
+			//EditorGUILayout.LabelField ("Assets/", GUILayout.MaxWidth (80f));
+			//EditorGUILayout.PropertyField (mSProp_strMeshPath, GUIContent.none);
+			EditorGUI.indentLevel--;
+			//EditorGUILayout.LabelField ("\\" + (mSProp_strMeshName.stringValue.Length == 0 ? "Mesh" : mSProp_strMeshName.stringValue));
+			GUILayout.EndHorizontal ();
+			EditorGUILayout.HelpBox ("The exported mesh will be saved as " 
+				+ (mSProp_strMeshName.stringValue.Length == 0 ? "meshedit_exportmesh" : mSProp_strMeshName.stringValue) + ".obj"
+				+ "\n\nExported mesh will be saved in the Assets main folder", MessageType.Info);
 			EditorGUI.indentLevel--;
 		}
 		EditorGUILayout.Space ();
 
 		// Commission Properties
 		EditorGUILayout.LabelField ("Commission Properties", EditorStyles.boldLabel);
-		GUI.color = new Color (1f, 0.25f, 0.25f);
-		if (GUILayout.Button ("Commit", GUILayout.MinHeight (40f)))
+		EditorGUILayout.HelpBox ("Click the button below to transform the gameObject. Once done, this script will be deleted", MessageType.Info);
+		GUI.color = new Color (1f, 0.6f, 0.25f);
+		if (mSProp_bIsExportMesh.boolValue)
+			if (GUILayout.Button ("Export Only"))
+			{
+				thisMeshedit.Export ();
+			}
+		if (GUILayout.Button (mSProp_bIsExportMesh.boolValue ? "Transform & Export" : "Transform"))
 		{
+			thisMeshedit.Export ();
 			thisMeshedit.Commit ();
 			return;
 		}
 		GUI.color = Color.white;
-		EditorGUILayout.HelpBox ("Click the button above to commit the changes to the gameObject. Once done, this script will be deleted", MessageType.Info);
 
 		serializedObject.ApplyModifiedProperties();
 	}
@@ -109,11 +165,14 @@ public class MesheditStepDrawer : PropertyDrawer
 		rectDrawer.width = fInspectorLength;
 		rectDrawer.width *= 0.2f;
 		EditorGUIUtility.labelWidth = 15f;
-		EditorGUI.PropertyField(rectDrawer, _serialisedProperty.FindPropertyRelative("m_fVariableX"), new GUIContent("X"));
+		_serialisedProperty.FindPropertyRelative("m_fVariableX").floatValue = 
+			EditorGUI.FloatField (rectDrawer, new GUIContent("X"), _serialisedProperty.FindPropertyRelative("m_fVariableX").floatValue);
 		rectDrawer.x += rectDrawer.width;
-		EditorGUI.PropertyField(rectDrawer, _serialisedProperty.FindPropertyRelative("m_fVariableY"), new GUIContent("Y"));
+		_serialisedProperty.FindPropertyRelative("m_fVariableY").floatValue = 
+			EditorGUI.FloatField (rectDrawer, new GUIContent("Y"), _serialisedProperty.FindPropertyRelative("m_fVariableY").floatValue);
 		rectDrawer.x += rectDrawer.width;
-		EditorGUI.PropertyField(rectDrawer, _serialisedProperty.FindPropertyRelative("m_fVariableZ"), new GUIContent("Z"));
+		_serialisedProperty.FindPropertyRelative("m_fVariableZ").floatValue = 
+			EditorGUI.FloatField (rectDrawer, new GUIContent("Z"), _serialisedProperty.FindPropertyRelative("m_fVariableZ").floatValue);
 		
 		EditorGUI.EndProperty();
 	}
--- DaburuTools/Assets/DaburuTools/Core/Meshedit/Meshedit.cs
+++ DaburuTools/Assets/DaburuTools/Core/Meshedit/Meshedit.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿#pragma warning disable 0414
+
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -10,9 +12,14 @@ public class Meshedit : MonoBehaviour
 
 	// Serialised Variables
 	[SerializeField] private List<MesheditStep> mList_mesheditStep = new List<MesheditStep>();
-	[SerializeField] private EnumPivotType m_enumPivotType = EnumPivotType.Origin;
+	[SerializeField] private EnumPivotType m_enumPivotType = EnumPivotType.Position;
+
+	[SerializeField] private string m_strMeshName = "meshedit_exportmesh";
+	//[SerializeField] private string m_strMeshPath = "Meshes";
 
 	[SerializeField] private bool m_bIsDisplayOutput = false;
+	[SerializeField] private bool m_bIsDisplayPivot = false;
+	[SerializeField] private bool m_bIsExportMesh = false;
 
 	// Un-serialised Variables
 	private Mesh m_mesh = null;
@@ -23,17 +30,23 @@ public class Meshedit : MonoBehaviour
 	{
 		if (m_bIsDisplayOutput) 
 		{
-			CalculateOutputAndUpdate ();
+			Vector3 vec3Origin = m_enumPivotType == EnumPivotType.Origin ? Vector3.zero : transform.position;
 
+			// if: display preview is selected
 			if (m_meshOutput != null)
 			{
-				Vector3 vec3Origin = m_enumPivotType == EnumPivotType.Origin ? Vector3.zero : transform.position;
-				for (int i = 0; i < m_meshOutput.triangles.Length / 3; i++)
-				{
-					Gizmos.DrawLine (vec3Origin + m_meshOutput.vertices[m_meshOutput.triangles[i * 3]], vec3Origin + m_meshOutput.vertices[m_meshOutput.triangles[i * 3 + 1]]);
-					Gizmos.DrawLine (vec3Origin + m_meshOutput.vertices[m_meshOutput.triangles[i * 3 + 1]], vec3Origin + m_meshOutput.vertices[m_meshOutput.triangles[i * 3 + 2]]);
-					Gizmos.DrawLine (vec3Origin + m_meshOutput.vertices[m_meshOutput.triangles[i * 3+ 2]], vec3Origin + m_meshOutput.vertices[m_meshOutput.triangles[i * 3]]);
-				}
+				Gizmos.DrawWireMesh (m_meshOutput, vec3Origin);
+			}
+
+			// if: display pivot is selected
+			if (m_bIsDisplayPivot) 
+			{
+				Gizmos.color = Color.red;
+				Gizmos.DrawLine (vec3Origin, vec3Origin + Vector3.right * 100f);
+				Gizmos.color = Color.green;
+				Gizmos.DrawLine (vec3Origin, vec3Origin + Vector3.up * 100f);
+				Gizmos.color = Color.blue;
+				Gizmos.DrawLine (vec3Origin, vec3Origin + Vector3.forward * 100f);
 			}
 		}
 	}
@@ -48,6 +61,15 @@ public class Meshedit : MonoBehaviour
 				Debug.LogWarning (name + ".Meshedit: No MeshFilter detected on gameObject!", this.gameObject);
 				return;
 			}
+
+			// Variable Resets
+			m_enumPivotType = EnumPivotType.Origin;
+			m_strMeshName = null;
+			//m_strMeshPath = null;
+
+			m_bIsDisplayOutput = false;
+			m_bIsDisplayPivot = false;
+			m_bIsExportMesh = false;
 		}
 	}
 
@@ -63,7 +85,7 @@ public class Meshedit : MonoBehaviour
 
 	// Public Functions
 	/// <summary>
-	/// Calculates the final model after it runs through the edit steps
+	/// Calculates the output model after it runs through the mesh-edit steps
 	/// </summary>
 	/// <returns> Returns the calculated mesh </returns>
 	public Mesh CalculateOutput()
@@ -111,20 +133,43 @@ public class Meshedit : MonoBehaviour
 		}
 		meshOutput.vertices = arr_vec3OutputVertices;
 		meshOutput.normals = arr_vec3OutputNormals;
+		meshOutput.RecalculateBounds ();
 
 		return meshOutput;
 	}
 
 	/// <summary>
-	/// Calculates the final model and updates to class' new mesh. This will not override the existing mesh!
+	/// Calculates the final model and updates to class' expected mesh output. This will not override the existing mesh! Use to update display of the output model
 	/// </summary>
 	public void CalculateOutputAndUpdate()
 	{
 		m_meshOutput = CalculateOutput ();
 	}
 
+	public void Export()
+	{
+		if (m_bIsExportMesh) 
+		{
+			if (m_strMeshName == null)
+				m_strMeshName = "meshedit_exportmesh";
+			else if (m_strMeshName.Length == 0)
+				m_strMeshName = "meshedit_exportmesh";
+
+			m_meshOutput.name = m_strMeshName;
+
+//			if (m_strMeshPath == null)
+//			{
+				UnityEditor.AssetDatabase.CreateAsset (m_meshOutput, "Assets/" + m_strMeshName + ".obj");
+				Debug.Log (name + ".Meshedit.Export(): Exported mesh to Assets", this.gameObject);
+//			}
+//			else if (m_strMeshPath.Length == 0)
+//				UnityEditor.AssetDatabase.CreateAsset (m_meshOutput, "Assets/" + m_strMeshPath  + "/" + m_strMeshName + ".obj");
+
+		}
+	}
+
 	/// <summary>
-	/// Commit the changes to existing mesh
+	/// Commit the output model to the current gameObject
 	/// </summary>
 	public void Commit()
 	{
@@ -133,8 +178,15 @@ public class Meshedit : MonoBehaviour
 			Debug.LogWarning (name + ".Meshedit.Commit(): Trying to commit to an empty mesh! Reverting changes!");
 			return;
 		}
+
+		if (m_strMeshName == null)
+			m_strMeshName = "meshedit_exportmesh";
+		else if (m_strMeshName.Length == 0)
+			m_strMeshName = "meshedit_exportmesh";
+
+		m_meshOutput.name = m_strMeshName;
 		GetComponent<MeshFilter> ().mesh = m_meshOutput;
-		Debug.Log (name + ".Meshedit.Commit(): Commited the mesh of the gameObject. Removing this script from gameObject");
+		Debug.Log (name + ".Meshedit.Commit(): Commited the mesh of the gameObject. Removing this script from gameObject", this.gameObject);
 		DestroyImmediate (this);
 	}
 }
